de71fa35d57c8bd9cf96af122443e672a9f1b997
Sina MohammadZadeh <sina75mn@outlook.com>
2021-10-16 20:41:19 +0330

    add btn

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
+import "./styles/app.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
-import App from "./App";
-import "./styles/app.css";
+import {Route, Switch} from "react-router";
+import {SplashPage} from "./views/pages/landing/splash_page";
+import {routes} from "./core/constants";
+import {DashboardPage} from "./views/pages/dashboard/dashboard_page";
 
 ReactDOM.render(
 	<BrowserRouter>
-		<App/>
+		<Switch>
+			<Route component={SplashPage} path={routes.splash} exact/>
+			<Route component={DashboardPage} path={routes.dashboard} exact/>
+		</Switch>
 	</BrowserRouter>,
 
 	document.getElementById('root')
