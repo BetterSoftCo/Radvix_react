20843ea52ef2e0bb76b2797d2a145e030d8f4574
amirhooseindaliri <68181834+amirhooseindaliri@users.noreply.github.com>
2022-02-21 11:43:01 -0800

    create discusstion controller and model

---
+++ src/controllers/discussion/discusstion_controller.ts
@@ -0,0 +1,29 @@
+import { toast } from "react-toastify";
+import { AddDataReq } from "../../data/models/requests/data/add_data_req";
+import { AddDataResResult } from "../../data/models/responses/data/add_data_res";
+import { RemoteData } from "../../data/remotes/data/remote_data";
+export class DiscusstionController {
+  remote = new RemoteData();
+
+  createData(
+    body: AddDataReq,
+    action: (res: AddDataResResult) => any,
+    error: (res: any) => any
+  ) {
+    this.remote.createData(
+      body,
+      (res) => {
+        toast.success(`${res.message}`, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        action(res.result);
+      },
+      (err) => {
+        toast.error(`${err.message}`, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        error(err);
+      }
+    );
+  }
+}
