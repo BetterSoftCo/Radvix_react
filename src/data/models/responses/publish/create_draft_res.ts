e3470ffe3c9c4202aaa5c7e9d9635677bb536774
eliham <elham.babolhavaeji74@gmail.com>
2022-03-13 15:25:16 +0330

    changed drafts model

--- src/data/models/responses/publish/create_draft_res.ts
+++ src/data/models/responses/publish/create_draft_res.ts
@@ -5,31 +5,6 @@ export interface CreateDraftRes {
 }
 
 export interface CreateDraftResResult {
-    id?:                number;
-    priority?:          number;
-    name?:              string;
-    category?:          string;
-    submitAt?:          string;
-    creatorUserId?:     string;
-    creatorFirstName?:  string;
-    creatorLastName?:   string;
-    nextDraftUploader?: NextDraftUploader;
-    startDate?:         Date;
-    endDate?:           Date;
-    users?:             NextDraftUploader[];
-    drafts?:            Draft[];
-}
-
-export interface Draft {
-    researchId?:           number;
-    finalVersion?:         boolean;
-    nextPersonToWorkOnId?: string;
-    publicationId?:        number;
-}
-
-export interface NextDraftUploader {
-    id?:        string;
-    firstName?: string;
-    lastName?:  string;
-    image?:     string;
+    publicationId?: number;
+    draftId?:       number;
 }
