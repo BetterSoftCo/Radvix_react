a736db10d24f5e5e69f1d1ecb5831d4775744d69
eliham <elham.babolhavaeji74@gmail.com>
2022-03-14 16:41:26 +0330

    added publish remote and controller -->

--- src/controllers/publish/publish_controller.ts
+++ src/controllers/publish/publish_controller.ts
@@ -1,12 +1,14 @@
 import { toast } from "react-toastify";
 import { CreateDraftReq } from "../../data/models/requests/publish/create_draft_req";
 import { CreatePublishReq } from "../../data/models/requests/publish/create_publish_req";
+import { EditPublishReq } from "../../data/models/requests/publish/update_publish_req";
 import { AddUserResResult } from "../../data/models/responses/publish/add_user_res";
 import { CreateDraftResResult } from "../../data/models/responses/publish/create_draft_res";
 import { CreatePublishResResult } from "../../data/models/responses/publish/create_publish_res";
 import { GetAllPublishesResult } from "../../data/models/responses/publish/publishes_res";
 import { GetPublishByIDResult } from "../../data/models/responses/publish/publish_by_id_res";
 import { SearchPublishResResult } from "../../data/models/responses/publish/search_publish_res";
+import { EditPublishResResult } from "../../data/models/responses/publish/update_publish_res";
 import { RemotePublish } from "../../data/remotes/publish/remote_publish";
 import { store } from "../../data/store";
 export class publishController {
@@ -122,6 +124,26 @@ export class publishController {
           position: toast.POSITION.TOP_RIGHT,
         });
         error(err);
+      }
+    );
+  }
+  updatePublish(
+    body: EditPublishReq,
+    action: (res: EditPublishResResult) => any,
+    error: (res: any) => any
+  ) {
+    this.remote.updatePublish(
+      body,
+      (res) => {
+        toast.success(`${res.message}`, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        action(res.result!);
+      },
+      (err) => {
+        toast.error(`${err.message}`, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
       }
     );
   }
