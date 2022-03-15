b05613212f0c93d66ab863ec2c11b34867fb80e2
eliham <elham.babolhavaeji74@gmail.com>
2022-03-15 21:17:23 +0330

    added publish remote --> draft

--- src/data/remotes/publish/remote_publish.ts
+++ src/data/remotes/publish/remote_publish.ts
@@ -7,6 +7,7 @@ import { CreateDraftRes } from "../../models/responses/publish/create_draft_res"
 import { CreatePublishRes } from "../../models/responses/publish/create_publish_res";
 import { GetAllPublishes } from "../../models/responses/publish/publishes_res";
 import { GetPublishByID } from "../../models/responses/publish/publish_by_id_res";
+import { RemoveDraftRes } from "../../models/responses/publish/remove_draft_res";
 import { SearchPublishRes } from "../../models/responses/publish/search_publish_res";
 import { EditPublishRes } from "../../models/responses/publish/update_publish_res";
 
@@ -47,5 +48,8 @@ export class RemotePublish {
   }
   updatePublish(body:EditPublishReq,action: (res: EditPublishRes) => any,error: (res: any) => any){
     return HTTP.put("/Publish" , body).then((res) => action(res.data)).catch((err)=>{error(err)});
+  }
+  removeDraft(body:{draftId:number},action: (res: RemoveDraftRes) => any,error: (res: any) => any){
+    return HTTP.delete(`/Publish/?draftId=${body.draftId}`).then((res) => action(res.data)).catch((err)=>{error(err)});
   }
 }
